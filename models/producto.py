19ea74d9b4df8c4bb66b0e6e4f2fe05f1f35e299
cristianvargasippc-art <cristianvargasippc@gmail.com>
2026-08-05 21:12:02 -0400

    commit

--- models/producto.py
+++ models/producto.py
@@ -1,31 +1,32 @@
 class Producto:
     def __init__(self):
         """
-        
-        
+            Constructor de la clase Producto.
+
+            Esta método recibirá los atributos necesarios para representar un producto.
         """
-        
-        def mostrar(self):
-            """
-        
-            
-            """
-            print("Metodo mostrar () pendiente de implementar")
-        
-        def aumentar_stock(self):
-            """
-            Aumento del stock disponible
-            """
-            print("Metodo aumentar_stock () pendiente")
-        
-        def disminuir_stock(self):
-            """
+
+
+    def mostrar(self):
+        """
+            Muestra la información del producto.
+        """
+        print("Método mostrar() pendiente de implementar")
+
+    def aumentar_stock(self):
+        """
+        Aumenta el stock disponible.
+        """
+        print("Método aumentar_stock() pendiente")
+
+    def disminuir_stock(self):
+        """
             Disminuye el stock disponible
-            
-            """    
-            print("Metodo disminuir_stock() pendiente")
-            
-        def hay_stock(self):
-            """
-            VERIFICA SI EXISTE STOCK
-            """
+        """
+        print("Mpetodo disminuir_stock() pendiente")
+
+    def hay_stock(self):
+        """
+            Verifica si existe stock suficiente
+        """
+        print("Método hay_stock() pendiente")
